4e06c8ad181f3824864944b93b1a8e02ac18f26a
nebuss <subin272051@gmail.com>
2024-07-11 09:18:24 +0900

    test

---
+++ test.py
@@ -0,0 +1,54 @@
+var1 = [1, 2, 3]
+var2 = [4, 5, 6]
+var1 + var2
+
+str1 = 'Hello'
+str2 = 'World!'
+str1 +' ' + str2
+
+str1 + 1
+
+a=10
+b=3.3
+
+print("a + b =", a + b) # 덧셈
+print("a - b =", a - b) # 뺄셈
+print("a * b =", a * b) # 곱셈
+print("a / b =", a / b) # 나눗셈
+print("a % b =", a % b) # 나머지
+print("a // b =", a // b) # 몫
+print("a ** b =", a ** b) # 거듭제곱
+
+a=10
+b=20
+
+print('a==b'm a==b)
+print("a != b", a!=b)
+print("a<b": a<b)
+print("a>b": a>b)
+
+c= ((2**4) + (12453 // 7)) % 8
+d = ((9**7) / 12 ) * (36452%253)
+c
+d
+c<d
+
+
+x=2
+y= ~x
+
+bin(x)
+bin(y)
+
+
+z=3
+bin(z)
+bin(~z)
+~z
+
+t=-3
+u=~t
+bin(u)
+u
+w
+test
