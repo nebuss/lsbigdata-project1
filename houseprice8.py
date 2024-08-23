0be90aaaf961326131c246ad47b60da995d405ee
nebuss <subin272051@gmail.com>
2024-08-23 11:05:18 +0900

    회귀분석 0823

--- houseprice8.py
+++ houseprice8.py
@@ -1,6 +1,4 @@
-# 원하는 변수를 사용해서 회귀모델을 만들고, 제출할것!
-# 원하는 변수 2개
-# 회귀모델을 통한 집값 예측
+# 검증데이터, 훈련데이터 나눠서 성능 회귀 분석 측정
 
 # 필요한 패키지 불러오기
 import numpy as np
@@ -13,51 +11,61 @@ house_train=pd.read_csv("./data/houseprice/train.csv")
 house_test=pd.read_csv("./data/houseprice/test.csv")
 sub_df=pd.read_csv("./data/houseprice/sample_submission.csv")
 
-## 이상치 탐색
+house_train.shape
+house_test.shape
+
+# 더미변수 생성
+neighborhood_dummies = pd.get_dummies(
+    combine_df["Neighborhood"],
+    drop_first=True)
+
+
+
+## 이상치 탐색 여기다 하면안됨. 이상치가 제거되면서 인덱스가 변하기 때문이다.
+#  validation set 나누고 나서 이상치 제거 할것
 # house_train=house_train.query("GrLivArea <= 4500")
 
-## 회귀분석 적합(fit)하기
-# house_train["GrLivArea"]   # 판다스 시리즈
-# house_train[["GrLivArea"]] # 판다스 프레임
-house_train["Neighborhood"]
-neighborhood_dummies = pd.get_dummies(
-    house_train["Neighborhood"],
-    drop_first=True
-    )
-# pd.concat([df_a, df_b], axis=1)
-x= pd.concat([house_train[["GrLivArea", "GarageArea"]], 
-             neighborhood_dummies], axis=1)
-y = house_train["SalePrice"]
+
+# 더미변수 데이터를 train, test으로 분리
+train_dummies = neighborhood_dummies.iloc[:1460,]
+
+test_dummies = neighborhood_dummies.iloc[1460:,]
+test_dummies = test_dummies.reset_index(drop=True)
+
+# 필요한 변수들만 골라서 더미 데이터 합치기
+my_train = pd.concat([house_train[["SalePrice", "GrLivArea", "GarageArea"]],
+               train_dummies], axis=1)
+
+my_test_x = pd.concat([house_test[["GrLivArea", "GarageArea"]],
+               test_dummies], axis=1)
+
+# train 데이터의 길이
+train_n = len(my_train) # 1460
+
+## Validation 셋(모의고사 셋) 만들기
+np.random.seed(42)
+val_index = np.random.choice(np.arange(train_n), size = 438,
+                 replace = False) #30% 정도의 갯수를 랜덤으로 고르기.
+
+new_valid = my_train.loc[val_index]  # 30% 438개
+new_train = my_train.drop(val_index) # 70% 1022개
+
+# 이상치 탐색 및 삭제 (validation을 나눠서 여기서 이상치 지워도 상관없다.변함이 없다)
+new_train = new_train.query("GrLivArea <= 4500")
+
+# train 데이터에서 가격 분리
+train_x = new_train.iloc[:,1:]
+train_y = new_train[["SalePrice"]]
+
+valid_x = new_valid.iloc[:,1:]
+valid_y = new_valid[["SalePrice"]]
 
 # 선형 회귀 모델 생성
 model = LinearRegression()
 
 # 모델 학습
-model.fit(x, y)  # 자동으로 기울기, 절편 값을 구해줌
+model.fit(train_x, train_y)
 
-# 회귀 직선의 기울기와 절편
-model.coef_      # 기울기 a
-model.intercept_ # 절편 b
-
-neighborhood_dummies_test = pd.get_dummies(
-    house_test["Neighborhood"],
-    drop_first=True
-    )
-test_x= pd.concat([house_test[["GrLivArea", "GarageArea"]], 
-                   neighborhood_dummies_test], axis=1)
-test_x
-
-# 결측치 확인
-test_x["GrLivArea"].isna().sum()
-test_x["GarageArea"].isna().sum()
-test_x=test_x.fillna(house_test["GarageArea"].mean())
-
-pred_y=model.predict(test_x) # test 셋에 대한 집값
-pred_y
-
-# SalePrice 바꿔치기
-sub_df["SalePrice"] = pred_y
-sub_df
-
-# csv 파일로 내보내기
-sub_df.to_csv("./data/houseprice/sample_submission10.csv", index=False)
+# 성능 측정
+y_hat = model.predict(valid_x)
+np.mean(np.sqrt((valid_y-y_hat)**2)) #26265
